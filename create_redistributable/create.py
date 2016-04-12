3461d76571851ab0d40f9a6402303dcfa32655a9
Jason Miller <jason.miller@inl.gov>
2016-04-12 11:12:48 -0600

    because there are multiple packages with conflicting names across different operating systems, devise a list routine, and create better help when this case is observed

--- create_redistributable/create.py
+++ create_redistributable/create.py
@@ -117,19 +117,31 @@ Class for building RedHat based packages
 """
   def _get_requirements(self):
     # RPM based distros have different package names for 'fortran'
-    # so try and discover exactly which package that actually is
-    rpm_process = subprocess.Popen(['rpm', '-qa', 'gcc-*fortran'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    rpm_results = rpm_process.communicate()[0]
-    requirements = '-'.join(rpm_results.split('-')[:2])
-    if requirements is not '':
+    # and libX11-devel, so try and discover exactly which package
+    # that actually is
+    requirements = []
+    our_requirements = ['gcc-*fortran', 'lib*11-devel']
+    for index, item in enumerate(our_requirements):
+      rpm_process = subprocess.Popen(['rpm', '-qa', item], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+      rpm_results = rpm_process.communicate()[0]
+      if rpm_results:
+        our_requirements[index] = '-'.join(rpm_results.split('-')[:2])
+        requirements.append(our_requirements[index])
+    if len(requirements) == len(our_requirements):
       return requirements
     else:
-      print 'ERROR: I could not determine what fortran package to\n' \
-      'include in the list of requirements the redistributable\n' \
-      'will inform the user to install if they do not have it.\n\n' \
-      'This either means my regular expression no longer works,\n' \
-      'or you do not currently have a fortran compiler installed\n' \
-      'via YOUR PACKAGE MANAGER (yum, zypper, dnf, etc).'
+      print '\nERROR: While building the list of dependencies the final redistributable will\n' \
+        'require your users to install, I could not determine the correct package names\n' \
+        'to add. The following is that list:\n\n\t', \
+        ' '.join(set(our_requirements) - set(requirements)), \
+        '\n\nThe unfortunate reason this happens, is due to the different ways each OS\n' \
+        'using RPM as its package management system can name things differently, and\n' \
+        'I am simply searching for the wrong package.\n\n' \
+        'The easy fix, is to edit this script, find the line: "our_requirements = " and\n' \
+        'modify the contents of that list to match the correct package installed using:\n\n\t' \
+        'rpm -qa <package name>\n\n' \
+        'Or... perhaps you really do not have the above packages installed... In which\n' \
+        'case, simply installing that package and re-running this script will suffice.\n\n'
       return False
 
   def prepare_area(self):
@@ -146,7 +158,7 @@ Class for building RedHat based packages
             xml_string = xml_string.replace('<PACKAGES_DIR>', self.args.packages_dir)
             xml_string = xml_string.replace('<PACKAGES_BASENAME>', os.path.join(*[x for x in os.path.dirname(self.args.packages_dir).split(os.sep)]))
             xml_string = xml_string.replace('<PACKAGES_PARENT>', os.path.basename(self.args.packages_dir))
-            xml_string = xml_string.replace('<REQUIREMENTS>', requirements)
+            xml_string = xml_string.replace('<REQUIREMENTS>', ' '.join(requirements))
             tmp_file.write(xml_string)
       for directory in ['BUILD', 'BUILDROOT', 'RPMS', 'SRPMS', 'SOURCES']:
         os.makedirs(os.path.join(self.temp_dir, 'rpm', directory))
@@ -177,7 +189,7 @@ Class for building RedHat based packages
       # get major_version from spec file instead of arbitrarily setting it
       major_version = '1.1'
       shutil.move(os.path.join(self.temp_dir, 'RPMS/x86_64/',
-                               '-'.join[self.base_name, major_version, self.version] + '.x86_64.rpm'),
+                               '-'.join([self.base_name, major_version, self.version]) + '.x86_64.rpm'),
                   os.path.join(self.args.relative_path, self.redistributable_name))
       print 'Redistributable built and available at:', os.path.join(self.args.relative_path, self.redistributable_name)
       return True
