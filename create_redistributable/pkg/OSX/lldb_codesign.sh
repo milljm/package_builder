212dd5de5c083bba5c2d5a448e6acbde0706a759
Jason M Miller <Jason.Miller@inl.gov>
2019-02-11 09:10:40 -0700

    Update how we handle the code sign cert
    Check for the presence of moose-codesign before attempting
    to delete it.
    
    For now, see if we even need the code-signing cert. This is
    TEST! I suspect, the get-task-allow entittlement is all that
    is required when using LLDB (any LLDB. even ours).

--- create_redistributable/pkg/OSX/lldb_codesign.sh
+++ create_redistributable/pkg/OSX/lldb_codesign.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 # Check if user supplied code signing certificate
-if [ -d "<PACKAGES_DIR>/llvm-<LLVM>/codesign" ]; then
-    security delete-certificate -c `cat <PACKAGES_DIR>/llvm-<LLVM>/codesign/codesign_name`
-    sleep 3
+if [ -f "<PACKAGES_DIR>/llvm-<LLVM>/codesign/codesign_name" ]; then
+    # Detect if there is a moose_codesign cert already present
+    security find-certificate -c `cat "<PACKAGES_DIR>/llvm-<LLVM>/codesign/codesign_name"` 2>&1 >/dev/null
+    return_code=`echo $?`
+    # previous moose-codesign cert detected
+    if [ $return_code -eq 0 ]; then
+        security delete-certificate -c `cat <PACKAGES_DIR>/llvm-<LLVM>/codesign/codesign_name`
+        sleep 5
+    fi
     security add-trusted-cert -d -r trustRoot -k /Library/Keychains/System.keychain <PACKAGES_DIR>/llvm-<LLVM>/codesign/moose_codesign.cert
     osascript -e 'tell app "System Events" to display dialog "LLDB code-signing certificate has been installed. \n\nNote: It may be necessary to reboot your machine for kernel extensions to load this certificate. Click `More Info` for details on how code-signing works and why a reboot may be necessary." with title "LLDB code-signing Caveats" buttons {"More Info", "Close"} default button 2
         if button returned of result = "More Info" then
